90e9bdfa4f65d088925bb9ff9a4b51719959339e
Gaël Haméon <gael.hameon@gmail.com>
2020-11-18 00:04:33 +0100

    test adding log to vanillla remark-images
    to see if we see them in the build logs in vercel

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const copyLinkedFiles = require('remark-copy-linked-files');
-const images = require('remark-images');
+const images = require('./remark-plugin/images'); // version that logs something during build
 
 const MD_MEDIA_COPIES = 'md-media-copies';
 const destinationDir = path.join(__dirname, 'public', MD_MEDIA_COPIES);
@@ -14,7 +14,7 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [
-      [copyLinkedFiles, { destinationDir, staticPath: '/md-media-copies/' }],
+      // [copyLinkedFiles, { destinationDir, staticPath: '/md-media-copies/' }],
       images,
     ]
   }
